b253a4116bdc6ec9299cb1000d323741ba1b7baf
Mayreeel <kuwon15@gmail.com>
2023-05-27 02:51:33 +0900

    feat : 리뷰 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
+import Review from './pages/Review';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/review" element={<Review />} />
       </Routes>
     </Router>
   );
