3749ac326eaf4f011cb1023ec8ea6db16a12635b
goYoungOd <lyslys77@tukorea.ac.kr>
2023-05-27 04:42:27 +0900

    info 페이지 기능 구현

--- src/pages/InfoPage.jsx
+++ src/pages/InfoPage.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './InfoPage.css';
+import axios from 'axios';
 
 function InfoPage() {
   const [isUsing, setIsUsing] = useState(false);
   const [userCount, setUserCount] = useState(5); // 이용자수 초기 설정
+  const [infoData, setInfoData] = useState([]);
 
   const handleButtonClick = () => {
-    if (isUsing) {
+    if (infoData) {
       setUserCount((prevCount) => prevCount - 1); // 이용자수 감소
     } else {
       setUserCount((prevCount) => prevCount + 1); // 이용자수 증가
@@ -19,15 +21,25 @@ function InfoPage() {
     window.history.back();
   };
 
+  async function getData() {
+    try {
+      const res = await axios.get(`/api/gym/`);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, []);
+
   return (
     <div>
       <div className="gym-info">
         <div className="button-wrapper">
           <button className="back-button" onClick={handleBackButtonClick}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-              <path d="M0 0h24v24H0z" fill="none" />
-              <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
-            </svg>
+          &lt;
           </button>
           <img
             className="image"
@@ -37,18 +49,18 @@ function InfoPage() {
         </div>
 
         <div className="info-text">
-          <h2>헬로우짐</h2>
+          <h2>{infoData.gyn_name}</h2>
           <p>운영시간: 7:00~ 23:00</p>
-          <p>경기도 시흥시 정왕동 1734-7 KR</p>
+          <p>{infoData.address}</p>
           <p>031-504-0111</p>
-          <p>현재 이용자수: {userCount}</p>
-          <p>운동기구: </p>
+          <p>현재 이용자수: {infoData.current_user}</p>
+          <p>운동기구:{infoData.machine} </p>
           <br />
         </div>
         <hr className="horizontal-line" />
         <div className="btn-container">
           <button
-            className={isUsing ? 'button-end' : 'button-use'}
+            className={infoData ? 'button-end' : 'button-use'}
             onClick={handleButtonClick}
           >
             {isUsing ? '종료하기' : '이용하기'}
