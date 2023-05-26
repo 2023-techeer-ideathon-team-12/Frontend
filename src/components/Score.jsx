f6fa48e0cbdafad4045439e583515ab630d50cb9
Mayreeel <kuwon15@gmail.com>
2023-05-27 02:51:12 +0900

    feat : 별점 컴포넌트

---
+++ src/components/Score.jsx
@@ -0,0 +1,30 @@
+import favorite from '../assets/image/favorite.png';
+import nonfavorite from '../assets/image/nonfavorite.png';
+import ScoreChart from './ScoreChart';
+
+function Score() {
+  return (
+    <div>
+      <div>
+        <div className="flex">
+          {/*별점 */}
+          <div className="flex flex-col justify-center">
+            <span className=" text-5xl mb-4 mx-auto">4</span>
+            {/*별 */}
+            <div className="flex">
+              <img className="w-6 h-6" src={favorite} />
+              <img className="w-6 h-6" src={favorite} />
+              <img className="w-6 h-6" src={favorite} />
+              <img className="w-6 h-6" src={favorite} />
+              <img className="w-6 h-6" src={nonfavorite} />
+            </div>
+          </div>
+          {/*막대 그래프 */}
+          <ScoreChart />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Score;
