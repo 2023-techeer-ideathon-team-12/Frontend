3235af71e04028b14ba2ef2efd50268e78b01749
Dayeon-Hong <dayon0325@gmail.com>
2023-05-27 07:15:05 +0900

    feat: info 페이지 카운팅 수정

--- src/pages/InfoPage.jsx
+++ src/pages/InfoPage.jsx
@@ -4,33 +4,49 @@ import axios from 'axios';
 
 function InfoPage() {
   const [isUsing, setIsUsing] = useState(false);
-  // const [userCount, setUserCount] = useState(5); // 초기 이용자 수
   const [infoData, setInfoData] = useState({});
+  const id = 9;
 
-  const handleButtonClick = () => {
-    if (infoData.data) {
-      setUserCount((prevCount) => prevCount - 1); // 이용 종료 시 이용자 수 감소
-      try {
-         axios.post(`/api/v1/gyms/${infoData.data.id}/increase`, {
-          id: infoData.data.id,
-          currentUser: infoData.data.currentUser - 1,
-        });
-      } catch (error) {
-        console.error(error);
-      }
-    } else {
-      setUserCount((prevCount) => prevCount + 1); // 이용 시작 시 이용자 수 증가
-      try {
-       axios.post(`/api/v1/gyms/${infoData.data.id}/decrease`, {
-          id: infoData.data.id,
-          currentUser: infoData.data.currentUser + 1,
-        });
-      } catch (error) {
-        console.error(error);
-      }
+  const handleStartUsing = async () => {
+    setInfoData((prevData) => ({
+      ...prevData,
+      currentUser: prevData.currentUser + 1,
+    }));
+
+    try {
+      await axios.post(
+        `http://localhost:8080/api/v1/gyms/${infoData.id}/increase`,
+        {
+          id: infoData.id,
+          currentUser: infoData.currentUser + 1,
+        },
+      );
+    } catch (error) {
+      console.error(error);
     }
 
-    setIsUsing((prevState) => !prevState);
+    setIsUsing(true);
+  };
+
+  const handleStopUsing = async () => {
+    setInfoData((prevData) => ({
+      ...prevData,
+      currentUser: prevData.currentUser - 1,
+    }));
+
+    try {
+      await axios.post(
+        `http://localhost:8080/api/v1/gyms/${infoData.id}/decrease`,
+        {
+          id: infoData.id,
+          currentUser: infoData.currentUser - 1,
+        },
+      );
+    } catch (error) {
+      console.error(error);
+    }
+
+    setIsUsing(false);
   };
 
   const handleBackButtonClick = () => {
@@ -39,7 +55,7 @@ function InfoPage() {
 
   useEffect(() => {
     axios
-      .get('/api/gym')
+      .get('http://localhost:8080/api/gyms/' + id)
       .then(function (response) {
         console.log(response);
         setInfoData(response.data);
@@ -64,22 +80,27 @@ function InfoPage() {
         </div>
 
         <div className="info-text">
-          <h2>{infoData.gym_name}</h2>
+          <h2>{infoData.name}</h2>
           <p>운영시간: 7:00~ 23:00</p>
           <p>{infoData.address}</p>
           <p>031-504-0111</p>
-          <p>현재 이용자수: {infoData.current_user}</p>
+          <p>현재 이용자수: {infoData.currentUser}</p>
           <p>운동기구: {infoData.machine}</p>
+          <p>헬스장 혼잡도: {infoData.status}</p>
           <br />
         </div>
         <hr className="horizontal-line" />
         <div className="btn-container">
-          <button
-            className={infoData.data ? 'button-end' : 'button-use'}
-            onClick={handleButtonClick}
-          >
-            {isUsing ? '종료하기' : '이용하기'}
-          </button>
+          {!isUsing && (
+            <button className="button-use" onClick={handleStartUsing}>
+              이용하기
+            </button>
+          )}
+          {isUsing && (
+            <button className="button-end" onClick={handleStopUsing}>
+              종료하기
+            </button>
+          )}
         </div>
       </div>
     </div>
